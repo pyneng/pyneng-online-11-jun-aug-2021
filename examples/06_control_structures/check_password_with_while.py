4e3de08516d1ce84e5a8c429d09d741ac539a20d
pyneng <pyneng.course@gmail.com>
2021-06-19 09:19:33 +0000

    update check_password_with_while.py

--- examples/06_control_structures/check_password_with_while.py
+++ examples/06_control_structures/check_password_with_while.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 username = input("Введите имя пользователя: ")
 password = input("Введите пароль: ")
 
@@ -9,23 +7,24 @@ while not password_correct:
     if len(password) < 8:
         print("Пароль слишком короткий\n")
         password = input("Введите пароль еще раз: ")
-    elif username in password:
+    elif username.lower() in password.lower():
         print("Пароль содержит имя пользователя\n")
         password = input("Введите пароль еще раз: ")
+    elif len(set("0123456789") & set(password)) < 3:
+        print("В пароле должны быть min 3 уникальных цифры\n")
+        password = input("Введите пароль еще раз: ")
     else:
-        print("Пароль для пользователя {} установлен".format(username))
+        print(f"Пароль для {username} прошел все проверки")
         password_correct = True
 
-"""
-Example:
-$ python check_password_with_while.py
-Введите имя пользователя: nata
-Введите пароль: nata
-Пароль слишком короткий
+print("#"*40)
 
-Введите пароль еще раз: natanata
-Пароль содержит имя пользователя
+# while (
+#     len(password) < 8 or username.lower() in password.lower()
+#     or len(set("0123456789") & set(password)) < 3
+# ):
+#     print(f"Пароль для {username} не прошел проверки")
+#     password = input("Введите пароль еще раз: ")
+# 
+# print(f"Пароль для {username} прошел все проверки")
 
-Введите пароль еще раз: 123345345345
-Пароль для пользователя nata установлен
-"""
